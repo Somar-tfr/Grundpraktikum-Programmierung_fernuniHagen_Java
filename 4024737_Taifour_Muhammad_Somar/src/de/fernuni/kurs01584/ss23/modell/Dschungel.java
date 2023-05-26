aecf875990e0f84a5c9978ed583528e53cdf9b2f
Somar <soumar_taifour@hotmail.com>
2023-05-26 13:57:48 +0200

    Funktionen getFeld, test Funktionen print, main ergänzt.

--- 4024737_Taifour_Muhammad_Somar/src/de/fernuni/kurs01584/ss23/modell/Dschungel.java
+++ 4024737_Taifour_Muhammad_Somar/src/de/fernuni/kurs01584/ss23/modell/Dschungel.java
@@ -1,9 +1,12 @@
 package de.fernuni.kurs01584.ss23.modell;
 
+import java.util.ArrayList;
+
 public class Dschungel {
 	private int zeilen;
 	private int spalten;
 	private String zeichenMenge;
+	private ArrayList<ArrayList<Feld>> dschungelMatrix;
 	
 	// TODO: Konstruktoren
 	public Dschungel(int zeilen, int spalten,String zeichenMenge) {
@@ -14,6 +17,20 @@ public class Dschungel {
 		this.zeilen = zeilen;
 		this.spalten = spalten;
 		this.zeichenMenge = zeichenMenge;
+		
+		this.dschungelMatrix = new ArrayList<>();
+		
+		for(int i = 0 ; i < zeilen; i++) {
+			ArrayList<Feld> zeile = new ArrayList<>();
+			
+			for (int j = 0; j< spalten; j++) {
+				Feld feld = new Feld(i,j);
+				feld.setId(spalten);
+				zeile.add(feld);
+			}
+			dschungelMatrix.add(zeile);
+		}
+		
 	}
 	
 	//getters
@@ -31,4 +48,26 @@ public class Dschungel {
 	}
 	
 	// TODO: Methoden
+	public Feld getFeld(int zNummer, int sNummer) {
+		return dschungelMatrix.get(zNummer).get(sNummer);
+	}
+	
+	
+	//a test method
+	public void print(/*String zeichen*/) {
+		for(ArrayList<Feld> zeile : dschungelMatrix) {
+			for (Feld element : zeile) {
+				System.out.print(element.getId());
+				}
+			
+			System.out.println();
+		}
+	}
+	//a test main
+	public static void main(String[] args) {
+		Dschungel test = new Dschungel(5,6, "abcd");
+		test.print();
+		System.out.println(test.getFeld(2,1));
+		
+	}
 }
